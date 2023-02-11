da485a0bc478350a2be15f2e8080de86b67a1774
buckboat <123005977+buckboat@users.noreply.github.com>
2023-02-10 20:15:50 -0600

    Added Game Logic

--- Sweepin With Friends/Assets/Script/Game.cs
+++ Sweepin With Friends/Assets/Script/Game.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public int width = 16;
+    public int height = 16;
+
+    private Board board;
+    private Tile[,] state;
+
+    private void Awake()
     {
-        
+        board = GetComponentInChildren<Board>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        NewGame();
     }
+
+    private void NewGame()
+    {
+        state = new Tile[width, height];
+
+        GenerateCells();
+
+        board.Draw(state);
+    }   
+
+    private void GenerateCells()
+    {
+        for(int x = 0; x < width; x++) {
+            for(int y =0; y < height; y++)
+            {
+                Tile tile = new Tile();
+                tile.position = new Vector3Int(x, y, 0);
+                tile.type = Tile.Type.Empty;
+                state[x, y] = tile;
+            }    
+        }
+    }
+
 }
