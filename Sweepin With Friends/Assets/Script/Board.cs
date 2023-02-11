4ce6b457cfa44ff9f0a3bdebb85a4100dc3116ff
buckboat <123005977+buckboat@users.noreply.github.com>
2023-02-10 20:02:04 -0600

    Error with Board Script
    Does not let me reference in unity

--- Sweepin With Friends/Assets/Script/Board.cs
+++ Sweepin With Friends/Assets/Script/Board.cs
@@ -7,12 +7,12 @@ public class Board : MonoBehaviour
 
     public Tilemap tilemap { get; private set; }
 
-    public Tile tileUnkown;
-    public Tile tileEmpty;
-    public Tile tileMine;
-    public Tile tileExploded;
-    public Tile tileFlag;
-    public Tile tileNum1;
+    public Tile tileUnknown; // untouched tile
+    public Tile tileEmpty; // empty tile
+    public Tile tileMine; // mine tile
+    public Tile tileExploded; // exploded mine
+    public Tile tileFlag; // flag tile
+    public Tile tileNum1; // number tile
     public Tile tileNum2;
     public Tile tileNum3;
     public Tile tileNum4;
@@ -23,13 +23,13 @@ public class Board : MonoBehaviour
     public Tile tileNum9;
     
 
-    private void Awake()
+    private void Awake() // creates map on activation
     {
 
-        //tilemap = GetComponent<tilemap>();
+        tilemap = GetComponent<Tilemap>();
     }
 
-    public void Draw(Tile[,] state)
+    public void Draw(Tile[,] state) // creates map based on size of board
     {
         int width = state.GetLength(0);
         int height = state.GetLength(1);
@@ -38,30 +38,65 @@ public class Board : MonoBehaviour
         {
             for(int y = 0; x < height; x++)
             {
-                Tile cell = state[x, y];
-               // tilemap.SetTile(cell.position, GetTile(cell));
+                Tile tile = state[x, y];
+                //tilemap.SetTile(tile.position, GetTile(tile)); // throws error?!
             }
+        }
+
+    }
+    
+    private Tile GetTile(Tile tile) // returns the tile image
+    {
+        if (tile.hidden)
+        {
+            return tileUnknown;
+        }
+        else if (tile.flagged)
+        {
+            return tileFlag;
+        }
+        else
+        {
+            return GetKnownTile(tile);
+        }
+
+    }
+
+    private Tile GetKnownTile(Tile tile) // returns the tile image other than unknown or flagged
+    {
+        switch (tile.type)
+        {
+            case Tile.Type.Empty: return tileEmpty;
+            case Tile.Type.Mine: return tileMine;
+            case Tile.Type.Number: return GetNumberTile(tile);
+            default: return tileNum9;
+
+        }
+    }
+
+    private Tile GetNumberTile(Tile tile) // returns the tile image for numbers
+    {
+        switch (tile.number)
+        {
+            case 1: return tileNum1;
+            case 2: return tileNum2;
+            case 3: return tileNum3;
+            case 4: return tileNum4;
+            case 5: return tileNum5;
+            case 6: return tileNum6;
+            case 7: return tileNum7;
+            case 8: return tileNum8;
+            case 9: return tileNum9;
+            default: return tileNum9;
         }
 
     }
 
 }
-/*
-private Tile GetTile(Tile tile)
-{
-    if (cell.hidden) 
-    {
 
-    } 
-    else if (cell.flagged)
-    {
 
-    } 
-    else
-    {
 
-    } 
-    
-} 
-*/
+
+
+
 
---
+++ Sweepin With Friends/Assets/Script/TIle.cs
@@ -0,0 +1,20 @@
+
+using UnityEngine;
+
+public struct Tile
+{
+
+    public enum Type
+    { 
+        Empty, // tile is empty
+        Mine, // tile is a mine
+        Number, // number of mines touching tile
+    }
+
+    public Vector3Int position; // position on board
+    public Type type; // type of tile
+    public int number; // number of mines touching tile
+    public bool hidden, flagged, hit;  // apearance of tile
+
+
+}
