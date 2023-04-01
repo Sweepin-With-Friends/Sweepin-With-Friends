a5c6118c3a239e011aef4b5ebace92ff1605aa08
Ken Hook <arkhamartorias@gmail.com>
2023-04-01 17:23:24 -0500

    Some Progress

---
+++ Sweepin With Friends/Assets/Plugins/Record.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Record] (
+    [GameID]         INT      NOT NULL,
+    [GameType]       INT      NULL,
+    [PlayerOneScore] INT      NULL,
+    [WinLoss]        BIT      NULL,
+    [WinTime]        TIME (7) NULL,
+    CONSTRAINT [PK_Record] PRIMARY KEY CLUSTERED ([GameID] ASC),
+    CONSTRAINT [FK_Record_Game] FOREIGN KEY ([GameID]) REFERENCES [dbo].[Game] ([GameID])
+);
+
