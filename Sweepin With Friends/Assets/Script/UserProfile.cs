95e0a8f33bfa85d1b976c406067024486216b4b4
buckboat <123005977+buckboat@users.noreply.github.com>
2023-04-07 12:42:10 -0500

    Updated Mine on title page and added user class

---
+++ Sweepin With Friends/Assets/Script/UserProfile.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserProfile
+{
+    public int UserID;
+
+    public string Email;
+
+    public string Username;
+
+    public string AvatarUrl;
+
+    public string Salt;
+
+    public string PasswordHash;
+
+    public int GameID;
+
+
+}
